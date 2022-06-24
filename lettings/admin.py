202c5292da904cca7cf137bb7050db706ff226f4
AkaSan242 <Lucaka@icloud.com>
2022-06-25 01:46:20 +0200

    create app lettings

---
+++ lettings/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Letting, Address
+
+# Register your models here.
+
+
+@admin.register(Letting)
+class LettingAdmin(admin.ModelAdmin):
+    list_display = ("title", "address")
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ("number", "street", "city", "state", "zip_code", "country_iso_code")
+
+# Register your models here.
