50b221298c7d79c4a092e94a3237cd3d8f48ad8d
AkaSan242 <Lucaka@icloud.com>
2022-06-27 23:17:46 +0200

    add test for profiles_views in tests.py

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.urls import reverse
+from .models import Profiles
+import pytest
 
 # Create your tests here.
+
+@pytest.mark.django_db
+def test_index_url(client):
+    path = reverse('profiles_index')
+    assert path == "/profiles/"
+    response = client.get(path)
+    content = response.content.decode()
+    expected_content = "<h1>Profiles</h1>"
+    assert response.status_code == 200
+    assert expected_content in content
+
+
+@pytest.mark.django_db
+def test_profile_url(client):
+    User.objects.create(
+        username="JohnDoe",
+        first_name="John",
+        last_name="Doe",
+        email="johndoe@hotmail.com",
+        password="unknow"
+        )
+
+    Profiles.objects.create(
+        user=User.objects.get(id=1),
+        favorite_city="Paris"
+        )
+
+    path = reverse('profile', kwargs={'username':'JohnDoe'})
+    assert path == "/profiles/JohnDoe/"
+    response = client.get(path)
+    content = response.content.decode()
+    expected_content = "<h1>JohnDoe</h1>"
+    assert response.status_code == 200
+    assert expected_content in content
