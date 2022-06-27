c855df26c6c2f8f5a13f81a33f2aac39dbeacc05
AkaSan242 <Lucaka@icloud.com>
2022-06-27 23:25:51 +0200

    add test for index view in tests.py

--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,11 @@
-def test_dummy():
-    assert 1
+from django.urls import reverse
+
+# Create your tests here.
+def test_index_url(client):
+    path = reverse('index')
+    assert path == "/"
+    response = client.get(path)
+    content = response.content.decode()
+    expected_content = "<h1>Welcome to Holiday Homes</h1>"
+    assert response.status_code == 200
+    assert expected_content in content
