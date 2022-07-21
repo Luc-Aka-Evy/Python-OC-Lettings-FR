81327f56f951c446f157e4c0c8e9d79b3ccf4361
AkaSan242 <Lucaka@icloud.com>
2022-07-21 18:41:07 +0200

    Update settings.py

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -3,7 +3,7 @@ import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 
 sentry_sdk.init(
-    dsn="$SENTRY_DNS",
+    dsn=os.getenv('SENTRY_DNS'),
     integrations=[
         DjangoIntegration(),
     ],
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "$SECRET_KEY"
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
